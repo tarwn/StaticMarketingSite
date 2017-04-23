3d016ba96f2ec854fc96f1a5d9cee3c1ec117ed4
Eli Weinstock-Herman <tarwn@tiernok.com>
2017-04-23 09:48:23 -0400

    More market-y landing page

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var drafts = require('metalsmith-drafts');
 var baseUrl = "http://devassure.com"
 
 Metalsmith(__dirname)
+  
+  // key/value metadata available for all files in my site
   .metadata({
     title: "My Static Site & Blog",
     description: "It's about saying »Hello« to the World.",
@@ -23,11 +25,21 @@ Metalsmith(__dirname)
       url: baseUrl
     }
   })
+
+  // process files from /src and output them to /build
   .source('./src')
   .destination('./build')
+
+  // clean the /build folder before rebuilding
   .clean(true)
+
+  // ignore blog posts with "draft: true"
   .use(drafts())
+
+  // process markdown files into HTML
   .use(markdown())
+
+  // define set of all posts and posts for landing page
   .use(collections({
     latestPosts: {
       pattern: 'posts/**/*.html',
@@ -41,25 +53,40 @@ Metalsmith(__dirname)
       reverse: true
     }
   }))
+
+  // convert filename.md to filename/index.html 
   .use(permalinks())
+
+  // process handlebars in content files
   .use(hbtmd(handlebars, {
       pattern: '**/*.html'
   }))
+
+  // process handlebars in layout files
   .use(layouts({
     engine: 'handlebars'
   }))
+
+  // generate a sitemap file
   .use(sitemap({
     hostname: baseUrl,
+    // defaults that can be overridden in individual files
     changefreq: "monthly",
     lastmod: new Date(),
     omitIndex: true
   }))
+
+  // generate an RSS feed from the posts collection
   .use(feed({
     collection: "posts",
     limit: 10,
     destination: "rss.xml"
   }))
+
+  // swith to "true" for debug output
   .use(debug(false))
+
+  // and Go!
   .build(function(err, files) {
     if (err) { throw err; }
   });
